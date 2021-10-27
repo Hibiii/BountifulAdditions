3af0981ba8125f1d6f23bc6ae6647f30bb3960ce
Hibi <69405847+Hibiii@users.noreply.github.com>
2021-10-27 20:19:41 -0300

    Add option to remove the crosshair in third person cameras

---
+++ src/main/java/hibi/bountifuladditions/mixin/InGameHudMixin.java
@@ -0,0 +1,26 @@
+package hibi.bountifuladditions.mixin;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+	@Shadow @Final
+	private MinecraftClient client;
+	@Inject(
+		method = "method_2427()Z",
+		at = @At("HEAD"),
+		cancellable = true
+	)
+	private void hideXhair(CallbackInfoReturnable<Boolean> cbir) {
+		if(this.client.options.perspective != 0)
+			cbir.setReturnValue(false);
+	}
+}
