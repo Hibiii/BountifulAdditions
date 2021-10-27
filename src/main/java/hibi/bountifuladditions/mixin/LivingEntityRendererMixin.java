9c1fff62b84652fe7c4fc380f3c37ea873f81bba
Hibi <69405847+Hibiii@users.noreply.github.com>
2021-10-27 16:40:32 -0300

    Add own name display

---
+++ src/main/java/hibi/bountifuladditions/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,23 @@
+package hibi.bountifuladditions.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.entity.LivingEntity;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntityRenderer.class)
+public class LivingEntityRendererMixin {
+	@Inject(
+		method = "hasLabel",
+		at = @At("HEAD"),
+		cancellable = true
+	)
+	private void showName(LivingEntity entity, CallbackInfoReturnable<Boolean> cbir) {
+		if(entity == MinecraftClient.getInstance().player)
+			cbir.setReturnValue(MinecraftClient.isHudEnabled() && !entity.isInvisible());
+	}
+}
